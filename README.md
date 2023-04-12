36525a972aec484d56ac28d9e1309d837d9d75d1
karamsetty09 <sandeepsandy2530@gmail.com>
2023-04-13 09:04:09 +1000

    Modal display for signup option.

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Vue 3 + TypeScript + Vite
 
+Commands to Run Application:
+
+npm run dev
+
+Commands to Run inbuilt server:
+
+npm run server
+
+Hosting at github pages:
+
+
 This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
 
 ## Recommended IDE Setup
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import Navbar from './components/Navbar.vue';
+import { useModal } from './composables/modal';
+import { computed } from 'vue';
+
+const modal = useModal()
+
+const modalStyle = computed(() => {
+  return {
+    display: modal.show.value ? 'block' : 'none'
+  }
+})
 </script>
 
 <template>
+  <div class="modal" style="color: white;" :style="modalStyle">
+    <div class="modal-background">
+      <div class="modal-content">
+        <div id="modal"></div>
+      </div>
+    </div>
+    <button @click="modal.hideModal()" class="modal-close is-large"></button>
+  </div>
+
   <div class="section">
     <div class="container">
       <Navbar />
